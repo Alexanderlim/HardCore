ed760b1383989878f075d6820fe46b2f3f7d87d9
Martin Hung <martin.hung02@gmail.com>
2014-02-26 05:36:44 -0800

    Created Start Menu
    Set up start menu dialogue
    Created template for saving and loading character data

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,83 @@
 
 
 #include "dialogue.hpp"
+#include "creature.hpp"
 
 
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 
 
 using namespace std;
 
+// Creature player;
+
+
 Dialogue dialogue;
 vector<string> choices;
 
+void save() {
+    ofstream fout ("test");
+    
+    fout << 1 << 2 << "test" << endl;
+    
+    cout << "Save Successful" << endl;
+}
+
+void load() {
+    ifstream fin ("test");
+
+    int a, b;
+    fin >> a >> b;
+    cerr << a << b << endl;
+    
+    cout << "Load Successful" << endl;
+}
+
+int startGame() {
+    int command;
+    vector<string> menuOptions;
+    
+    menuOptions.push_back("New Game");
+    menuOptions.push_back("Load Game");
+    menuOptions.push_back("Etc");
+    
+    command = Dialogue("Main Menu", menuOptions).activate();
+    
+    switch (command) {
+        case 1: // New Game
+            break;
+        case 2: // Load Game
+            load();
+            break;
+        case 3: // Any Other options ie credits
+            break;
+        default:
+            return 0;
+            break;
+    }
+    return 1;
+    
+}
+
+
+
 int main(int argc, const char* argv[])
 {
-    // Testing out the Dialogue class
-    choices.push_back("Option 1");
-    choices.push_back("Option 2");
-    dialogue = Dialogue("Hello World", choices);
-    dialogue.activate();	
-
-    cout << "Hello World\n";
+    int command;
+    
+    // Main Loop
+    while (cin) {
+        
+        startGame();
+        
+        
+    }
+    
+    
+    
     return 0;
-
+}
